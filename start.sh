b4d0104cf98f2d86aac44888573513a61368fde2
ᴠ ɴ ᴄ s <vncscode@proton.me>
2024-06-24 14:05:11 -0300

    nxf-update

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+GREEN='\033[1;32m'
+BLUE='\033[0;34m'
+while : 
+do
+printf "${GREEN}︎TOGA-MD 3.0 -_ Sistema de reinício automático ligado! Iniciando bot...\n"
+if [ "$1" = "sim" ]; then
+node connect.js sim
+elif [ "$1" = "não" ]; then
+node connect.js não
+else 
+node connect.js
+fi
+sleep 1 
+printf "${BLUE}- O ︎programa fechado! Iniciando o projeto novamente, aguarde...\n"
+done
